f40b9d14811f3de483b42428c8a3a7e72bed8a8d
mattrbanks <banks.matt81@gmail.com>
2020-06-25 10:10:43 -0400

    Fixed remaining bad import for imageReusable component.

--- src/components/fourGridButtons.js
+++ src/components/fourGridButtons.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Card, useTheme, useMediaQuery } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
-import ImageReusable from "../components/ImageReusable"
+import ImageReusable from "../components/imageReusable"
 import { Link } from "gatsby"
 
 const useStyles = makeStyles(theme => ({
