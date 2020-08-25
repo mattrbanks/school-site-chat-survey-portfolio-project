ebc69c5159c22066b86cd71796c3af6915102a23
mattrbanks <banks.matt81@gmail.com>
2020-08-25 00:33:23 -0400

    The footer is fixed for mobile.

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,6 +13,7 @@ import Header from "./header"
 import "./layout.css"
 import Menu from "./menu"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -29,72 +30,57 @@ const Layout = ({ children }) => {
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
       <Menu />
-      <div
-        style={
-          {
-            //margin: `0 auto`,
-            //maxWidth: 1700,
-            //padding: `0 1.0875rem 1.45rem`,
-          }
-        }
-      >
+      <div>
         <main>{children}</main>
-        <footer style={{ fontSize: "0.8rem" }}>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-evenly",
-            }}
-          >
-            <div style={{ marginTop: "1rem" }}>
-              <h4 style={{ marginLeft: "1rem" }}>Eagle Elementary</h4>
-              <ul style={{ listStyle: "none" }}>
-                <li>
-                  This is a template <br />
-                  school site.
-                </li>
-                <li>
-                  This site is for a <br />
-                  portfolio.
-                </li>
-                <li>
-                  This site was <br />
-                  developed by <br />
-                  Matthew Banks.
-                </li>
-              </ul>
+        <footer style={{ fontSize: "0.95rem" }}>
+          <FooterStyles>
+            <div className={"footer-info mobile-footer-p mobile-footer-l"}>
+              <div style={{ marginTop: "1rem" }}>
+                <h4 style={{ marginLeft: "1rem", marginRight: "1.45rem" }}>
+                  Eagle Elementary
+                </h4>
+                <ul style={{ listStyle: "none", marginRight: "1.45rem" }}>
+                  <li>This is a template school site.</li>
+                  <li>This site is for a portfolio.</li>
+                  <li>This site was developed by Matthew Banks.</li>
+                </ul>
+              </div>
+              <div style={{ marginTop: "1rem" }}>
+                <h4 style={{ marginLeft: "1rem", marginRight: "1.45rem" }}>
+                  Contact
+                </h4>
+                <ul style={{ listStyle: "none", marginRight: "1.45rem" }}>
+                  <li>
+                    <Link to="https://github.com/mattrbanks">Github</Link>
+                  </li>
+                  <li>
+                    <Link to="https://www.linkedin.com/in/matthewr-banks">
+                      LinkedIn
+                    </Link>
+                  </li>
+                  <li>
+                    <a href="mailto:banks.matt81@gmail.com">Email</a>
+                  </li>
+                </ul>
+              </div>
+              <div style={{ marginTop: "1rem" }}>
+                <h4 style={{ marginLeft: "1rem", marginRight: "1.45rem" }}>
+                  Links
+                </h4>
+                <ul style={{ listStyle: "none", marginRight: "1.45rem" }}>
+                  <li>
+                    <Link to="/curriculum">Our curriculum</Link>
+                  </li>
+                  <li>
+                    <Link to="/events">Events</Link>
+                  </li>
+                  <li>
+                    <Link to="/principals-message">Principals message</Link>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div style={{ marginTop: "1rem" }}>
-              <h4 style={{ marginLeft: "1rem" }}>Contact</h4>
-              <ul style={{ listStyle: "none" }}>
-                <li>
-                  <Link to="https://github.com/mattrbanks">Github</Link>
-                </li>
-                <li>
-                  <Link to="https://www.linkedin.com/in/matthewr-banks">
-                    LinkedIn
-                  </Link>
-                </li>
-                <li>
-                  <a href="mailto:banks.matt81@gmail.com">Email</a>
-                </li>
-              </ul>
-            </div>
-            <div style={{ marginTop: "1rem" }}>
-              <h4 style={{ marginLeft: "1rem" }}>Links</h4>
-              <ul style={{ listStyle: "none" }}>
-                <li>
-                  <Link to="/curriculum">Our curriculum</Link>
-                </li>
-                <li>
-                  <Link to="/events">Events</Link>
-                </li>
-                <li>
-                  <Link to="/principals-message">Principals message</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
+          </FooterStyles>
           <div>
             <h5
               style={{
@@ -120,3 +106,24 @@ Layout.propTypes = {
 }
 
 export default Layout
+
+let FooterStyles = styled.div`
+  .footer-info {
+    display: flex;
+    justify-content: space-evenly;
+  }
+  @media (max-width: 500px) and (orientation: portrait) {
+    .mobile-footer-p {
+      display: block;
+      text-align: center;
+      padding-top: 1rem;
+    }
+  }
+  @media (max-width: 900px) and (orientation: landscape) {
+    .mobile-footer-l {
+      display: block;
+      text-align: center;
+      padding-top: 1rem;
+    }
+  }
+`
